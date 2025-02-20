d1da098d69feb89f9a36726f2180cc8bac53c32f
epilihP <philipegcustodio.com>
2025-02-20 19:51:48 -0300

    new cadastro

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-lista=[]
-#lista
+chamados=[]
+id_cliente= 1
+
+def cadastrar_chamados (descricao, prioridade):
+    global id_cliente
+    chamado ={
+        "id": id_cliente,
+        "descricao" : descricao,
+        "prioridade" : prioridade,
+        "finalizado" : False
+    }
+    chamados.append(chamado)
+    id_clientes += 1
+    print(f"Chamado{chamado[id]} cadastrado com sucesso." )
